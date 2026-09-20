f49af34c148b57d8c0409ee3f02b9596b1a440c6
benjamin <ebenjaminhd11@gmail.com>
2026-09-19 23:09:19 -0500

    feat: add auth serializers

---
+++ consultor/serializers/user_serializer.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "is_active",
+            "date_joined",
+        ]
+        read_only_fields = [
+            "id",
+            "email",
+            "is_active",
+            "date_joined",
+        ]
