69ccf530bc159e184d85c20ae5dc7e627f3b5ac3
benjamin <ebenjaminhd11@gmail.com>
2026-09-19 23:42:19 -0500

    feat: add authentication views and routes

--- consultor/views/__init__.py
+++ consultor/views/__init__.py
@@ -1,2 +1,6 @@
 from .ask_view import AskView
 from .doubt_view import DoubtView
+from .login_view import LoginView
+from .me_view import UserMeView
+from .refesh_token_view import CookieTokenRefreshView
+from .register_view import RegisterView
